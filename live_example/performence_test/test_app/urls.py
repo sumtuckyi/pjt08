22ca6235b0b6b8854cfeaa1ceaf4c857e69dea9c
이수민 <lsmmuseum@gmail.com>
2023-11-03 14:02:30 +0900

    11/3

--- live_example/performence_test/test_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('normal_sort/', views.normal_sort),
-    path('priority_queue/', views.priority_queue),
-    path('bubble_sort/', views.bubble_sort),
-]
-
--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 import random
+import pandas as pd
+
 
 array_length = 1000
 random_range = 5000
@@ -41,3 +43,17 @@ def priority_queue(request):
         'top': -pq.get()
     }
     return JsonResponse(context)
+
+@api_view(['GET'])
+def dataFrame(request):
+    df = pd.read_csv('data/test_data_has_null.CSV', encoding='cp949')
+    
+    df.fillna("NULL", inplace=True)
+    new_df = df[df['나이'] != "NULL"] 
+    average = new_df['나이'].mean()
+    new_df['difference'] = abs(new_df['나이'] - average)
+    df = new_df.sort_values(by='difference')
+    nearest_10_rows = df.head(10)
+
+    data = nearest_10_rows.to_dict('records')
+    return JsonResponse({'dat': data})
